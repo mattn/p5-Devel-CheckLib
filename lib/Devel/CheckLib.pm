1b7beb22b1a059d2ada243d51f64d785a9b96095
mattn <mattn.jp@gmail.com>
2015-01-04 11:10:51 +0900

    bump-up Version

--- lib/Devel/CheckLib.pm
+++ lib/Devel/CheckLib.pm
@@ -5,7 +5,7 @@ package Devel::CheckLib;
 use 5.00405; #postfix foreach
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = '1.02';
+$VERSION = '1.03';
 use Config qw(%Config);
 use Text::ParseWords 'quotewords';
 
