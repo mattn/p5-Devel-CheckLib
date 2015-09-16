a320ad69a061bfa6bb17b93e315dd8c30f3db057
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-09-16 09:37:41 +0000

    bump

--- lib/Devel/CheckLib.pm
+++ lib/Devel/CheckLib.pm
@@ -5,7 +5,7 @@ package Devel::CheckLib;
 use 5.00405; #postfix foreach
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
-$VERSION = '1.03';
+$VERSION = '1.04';
 use Config qw(%Config);
 use Text::ParseWords 'quotewords';
 
