48edea9bb209081ff42744398cb22f6028a7dede
Todd Rinaldo <toddr@cpan.org>
2017-03-21 21:27:05 -0500

    Ignore linker options when found in @ldflags

--- lib/Devel/CheckLib.pm
+++ lib/Devel/CheckLib.pm
@@ -473,7 +473,7 @@ sub _findcc {
         push @Config_ldflags, $config_val if ( $config_val =~ /\S/ );
     }
     my @ccflags = grep { length } quotewords('\s+', 1, $Config_ccflags||'', $user_ccflags||'');
-    my @ldflags = grep { length } quotewords('\s+', 1, @Config_ldflags, $user_ldflags||'');
+    my @ldflags = grep { length && $_ !~ m/^-Wl/ } quotewords('\s+', 1, @Config_ldflags, $user_ldflags||'');
     my @paths = split(/$Config{path_sep}/, $ENV{PATH});
     my @cc = split(/\s+/, $Config{cc});
     if (check_compiler ($cc[0], $debug)) {
